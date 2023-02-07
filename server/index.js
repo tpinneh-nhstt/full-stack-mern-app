eb12cf841b7b71a136397b9709bea9cac29ba0b0
Tuborlaifah <tuborlaifah.pinneh@test-and-trace.nhs.uk>
2023-02-07 13:26:22 +0000

    Backend Complete, Mock Data Inserted

--- server/index.js
+++ server/index.js
@@ -12,8 +12,11 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import { createPost } from "./controllers/posts.js"
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGS */
 const __filename = fileURLToPath(import.meta.url);
@@ -56,5 +59,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    /* INPUT DATA ONCE */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
 
